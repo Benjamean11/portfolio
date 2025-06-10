732989dbdf310a9b45f5d7abd37e3dcb8ed759d8
Benjamean11 <benjaminushie50@gmail.com>
2025-06-10 13:09:49 +0100

    update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-     <div>
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <Projects></Projects>
-      <Contact></Contact>
-     </div>
+    <div>
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
 export default App;
+
 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import '../index.css'; 
+
+const Loader = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl text-blue-500 font-bold grow-shrink">
+        Benjamin...
+      </h1>
+    </div>
+  );
+};
+
+export default Loader;
